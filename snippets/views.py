6f4351a598be8489e0f75ab963321031db4ee162
Lennox Mountain <lennoxmountain67@gmail.com>
2024-08-03 11:22:56 +0200

    moving from function based to class based views

--- snippets/views.py
+++ snippets/views.py
@@ -1,31 +1,35 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
+#from django.shortcuts import render
+#from django.http import HttpResponse, JsonResponse
+#from django.views.decorators.csrf import csrf_exempt
+#from rest_framework.parsers import JSONParser
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 from rest_framework.decorators import api_view
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.http import Http404
 
 # Create your views here.
-@csrf_exempt
-@api_view(['GET', 'POST'])
-def snippet_list(request, format=None):
-    #lists all code snippets or create new snippet
 
-    if request.method == 'GET':
-        snippets  = Snippet.objects.all()
-        serializer = SnippetSerializer(snippets, many=True)
-        return Response(serializer.data)
+#function based 
+# @csrf_exempt
+# @api_view(['GET', 'POST'])
+# def snippet_list(request, format=None):
+#     #lists all code snippets or create new snippet
+
+#     if request.method == 'GET':
+#         snippets  = Snippet.objects.all()
+#         serializer = SnippetSerializer(snippets, many=True)
+#         return Response(serializer.data)
     
-    elif request.method == 'POST':
-        serializer = SnippetSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+#     elif request.method == 'POST':
+#         serializer = SnippetSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
         
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @csrf_exempt
 @api_view(['GET', 'PUT', 'DELETE'])
