e00ff4ceb67246985a583aeb1a66f355685ec1b5
Lennox Mountain <lennoxmountain67@gmail.com>
2024-08-04 20:00:08 +0200

    added permisions that allow only authenticated users to get read, write accesses while the lest get only r5ead access

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,15 +1,22 @@
 from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 from django.contrib.auth.models import User
+from rest_framework import permissions
 
 
 class UserSerializer(serializers.modelSerializer):
     snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
 
+    #permisions that allow only authenticated users to get read, write accesses while the lest get only r5ead access
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
     class Meta:
         model = User
         fields = ['id', 'username', 'snippet']
 class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     class Meta:
         model = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+        fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner']
