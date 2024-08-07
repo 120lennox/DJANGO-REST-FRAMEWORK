14004c2614c24f6ca648d4fc1fd77940180bbdf8
Lennox Mountain <lennoxmountain67@gmail.com>
2024-08-07 10:28:07 +0200

    added urls for new created views

--- snippets/views.py
+++ snippets/views.py
@@ -18,6 +18,7 @@ from rest_framework import permissions
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
+from rest_framework import renderers
 
 # Create your views here.
 
@@ -28,6 +29,16 @@ def api_root(request, format=True):
         'users': reverse('user-lost', request=request, format=format),
         'snippets': reverse('snippet-list', request=request, format=format)
     })
+
+#snippet highlighter endpoint
+class SnippetHighlight(generic.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+    
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
